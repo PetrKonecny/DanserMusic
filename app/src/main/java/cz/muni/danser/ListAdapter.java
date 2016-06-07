853818be4d375b9fcff279dab4b7bbdce325b0c9
melanger <melanger@users.noreply.github.com>
2016-06-07 22:28:06 +0200

    Made images work
    at least on 4.4

--- app/src/main/java/cz/muni/danser/ListAdapter.java
+++ app/src/main/java/cz/muni/danser/ListAdapter.java
@@ -1,26 +1,22 @@
 package cz.muni.danser;
 
 import android.content.Context;
-import android.media.Image;
-import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.activeandroid.query.Select;
 
 import java.util.List;
 
+import cz.muni.danser.model.DanceCategory;
 import cz.muni.danser.model.DanceSong;
 import cz.muni.danser.model.Listable;
 import cz.muni.danser.model.Playlist;
-import cz.muni.danser.model.SongPlaylist;
 import cz.muni.danser.model.StringParsable;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
@@ -61,12 +57,14 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTextView;
+        public ImageView mImageView;
         public CardView mCardView;
         public ImageButton mButton;
 
         public ViewHolder(View v) {
             super(v);
             mTextView = (TextView) v.findViewById(R.id.list_item_text_view);
+            mImageView = (ImageView) v.findViewById(R.id.category_image);
             mCardView = (CardView) v.findViewById(R.id.card_view);
             mButton = (ImageButton) v.findViewById(R.id.playlist_list_item_delete);
 
@@ -81,6 +79,30 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
                 text = item.getMainText();
             }
             mTextView.setText(text);
+            if(mImageView != null && item instanceof DanceCategory){
+                int id;
+                switch(((DanceCategory) item).getDanceCategory()){
+                    case "ballroom":
+                        id = R.drawable.vector_drawable_dance_category_ballroom;
+                        break;
+                    case "latin":
+                        id = R.drawable.vector_drawable_dance_category_latin;
+                        break;
+                    case "club":
+                        id = R.drawable.vector_drawable_dance_category_club;
+                        break;
+                    case "folk":
+                        id = R.drawable.vector_drawable_dance_category_folk;
+                        break;
+                    case "jazz":
+                        id = R.drawable.vector_drawable_dance_category_jazz;
+                        break;
+                    default:
+                        id = R.drawable.vector_drawable_dance_category_miscellaneous;
+                        break;
+                }
+                mImageView.setImageResource(id);
+            }
             mCardView.setOnClickListener(new View.OnClickListener() {
                 @Override public void onClick(View v) {
                     notifyItemChanged(selectedPos);
